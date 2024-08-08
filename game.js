0bc74ab297020a73ad9b0307d2f35e84de729f8b
Branden21 <statefan1324@gmail>
2024-08-07 21:29:28 -0400

    added support for different categories

--- game.js
+++ game.js
@@ -3,81 +3,93 @@ document.addEventListener('DOMContentLoaded', () => {
   const quizData = {
     easy: [
       {
+        // need to add more categories because when they run out they go to other categories
         question: "How do you become a Christian?",
         answers: ["The Apostle's Creed", "The Lord's Prayer", "The ten Commandments", "Beleive Christ died for you"],
         correct: "Beleive Christ died for you",
         reason: "Christian Formula = Christ was killed for us, His death has washed away our sins, and by dying he has disabled death itself. You are Christian by beleive Christ died for you.",
-        mode: "easy"
+        mode: "easy",
+        category: "Christian Life based in the Bible"
       },
       {
         question: "What is the last word in the Bible?",
         answers: ["Amen", "Jesus", "Israel", "name"],
         correct: "Amen",
         reason: "The last word in the bible is Amen.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "What town was Jesus born in?",
         answers: ["Bethlehem", "Jerusalem", "Israel", "Nazareth"],
         correct: "Bethlehem",
         reason: "In Matthew 2:1-2 it states that Jesus was born in Bethlehem.",
-        mode: "easy"
+        mode: "easy",
+        category: "History in the Bible"
       },
       {
         question: "Romans 3:23 says that who has sinned?",
         answers: ["Everyone", "The Devil", "Israel", "Peter"],
         correct: "Everyone",
         reason: "Romans 3:23 saids that everyone has sinned and fell short of the glory of God.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "Who did God tell to let my people go?",
         answers: ["Moses", "Jesus", "David", "Abraham"],
         correct: "Moses",
         reason: "In Exodus 5:1 God told Moses to let my people go, so that they may hold a festival unto me in the wilderness.",
-        mode: "easy"
+        mode: "easy",
+        category: "History in the Bible"
       },
       {
         question: "which books are the Gospels?",
         answers: ["Matthew, Mark, Luke, John", "Matthew, Mark, Luke, John, Peter", "Numbers", "All of them"],
         correct: "Matthew, Mark, Luke, John",
         reason: "The Gospels are the records we have of Jesus' life. Matthew, Mark, Luke, John wrote down the teachings of Jesus.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "What word or phrase means giving somebody something good that he or she doesn't deserve?",
         answers: ["Grace", "Forgiveness", "Love", "Kindness"],
         correct: "Grace",
         reason: "Grace is giving something good that he or she doesn't deserve.",
-        mode: "easy"
+        mode: "easy",
+        category: "Christian Life based in the Bible"
       },
       {
         question: "Who walked on water toward Jesus?",
         answers: ["Peter", "Matthew", "Andrew", "John"],
         correct: "Peter",
         reason: "Peter walked on water toward Jesus in Matthew 14:22-33.",
-        mode: "easy"
+        mode: "easy",
+        category: "History in the Bible"
       },
       {
         question: "Fill in the blank: Thy word is a ____ unto my feet, and a light unto my path.",
         answers: ["Lamp", "light", "Beacon", "Guide"],
         correct: "Lamp",
         reason: "Thy word is a lamp unto my feet is in Psalm 119:105 in King James Version of the Bible.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "From what part of Adam's body did God create Eve?",
         answers: ["thigh", "Gluteus Maximus", "Rib", "Elbow"],
         correct: "Rib",
         reason: "God created Eve with Adam's Rib in Genesis 2:21-22.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "What is verse 1 in Genesis?",
         answers: ["In the beginning God created the heavens and the earth", "In the beginning God created light from darkness", "Amen. Come, Lord Jesus", "And God said, Let there be light"],
         correct: "In the beginning God created the heavens and the earth",
         reason: "The first verse in the Bible is in the beginning God created the heavens and the earth.",
-        mode: "easy"
+        mode: "easy",
+        category: "Common Facts about Bible"
       },
       {
         question: "Who is called the light of the world?",
@@ -85,7 +97,8 @@ document.addEventListener('DOMContentLoaded', () => {
         correct: "Jesus",
         reason: "Jesus is called the light of the world in John 8:12.",
         mode: "easy",
-        image: "images/jesus.jpg"
+        image: "images/jesus.jpg",
+        category: "Christian Life based in the Bible"
       },
       {
         question: "What is the duty of man, according to the Scriptures?",
@@ -93,14 +106,16 @@ document.addEventListener('DOMContentLoaded', () => {
         correct: "Fear God and keep his commandments",
         reason: "The duty of man in the scriptures is to Fear God and keep his commandments stated in Ecclesiastes 12:13.",
         mode: "easy",
-        image: "images/prayer.jpg"
+        image: "images/prayer.jpg",
+        category: "Christian Life based in the Bible"
       },
       {
         question: "What was the first miracle performed by Christ?",
         answers: ["Healing of a blind man","Feeding of the five thousand", "Water turned into wine", "Jesus walking on water"],
         correct: "Water turned into wine",
         reason: "The first miracle Christ performed was turning water into wine in John 2:1.",
-        mode: "easy"
+        mode: "easy",
+        category: "History in the Bible"
       },
     
     ],
@@ -318,8 +333,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentQuestionIndex = 0;
   let difficulty = 'easy'; // Initial difficulty level
   let questionsAnswered = 0;
-  let categorySelect = document.getElementById('category');
-  const difficultySelect = document.getElementById('difficulty');
   const questionText = document.getElementById('question-text');
   const answersContainer = document.getElementById('answers-container');
   const nextButton = document.getElementById('next-button');
@@ -329,10 +342,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const questionImage = document.getElementById('question-image');
   let points = 0;
   // shuffles questions based on difficulty
-  let questionsPool = shuffleArray(quizData[difficulty]);
+  //let questionsPool = shuffleArray(quizData[difficulty]);
   
   // Retrieve difficulty from local storage
   const savedDifficulty = localStorage.getItem('selectedDifficulty');
+  const savedCategory = localStorage.getItem('selectedCategory');
   difficulty = savedDifficulty || 'easy'; // Default to 'easy' if no saved difficulty
   
 
@@ -343,13 +357,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // show buttons initially
   answersContainer.style.visibility = "visible";
 
-  if (savedDifficulty === 'all') {
-    adjustDifficulty();
-  } else {
-    // Filter questions based on selected difficulty
-    questionsPool = shuffleArray(quizData[difficulty]);
-  }
-
   // Function to shuffle array elements
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -359,6 +366,15 @@ document.addEventListener('DOMContentLoaded', () => {
     return array;
   }
   
+  // Function to filter questions based on selected category
+  function getFilteredQuestions() {
+    return shuffleArray(quizData[difficulty].filter(question => {
+      return savedCategory === 'all' ? true : question.category === savedCategory;
+    }));
+  }
+
+  // Initial filtering of questions
+  let questionsPool = getFilteredQuestions();
   // Function to load a question and its answers
   function loadQuestion() {
     if (questionsPool.length === 0) {
